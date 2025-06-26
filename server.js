43794954ef0df553eb8e47631c58bc0e63ec9d23
himanshumvm <himanshusharma.mvmbs@gmail.com>
2025-06-26 11:00:00 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import categoryRoutes from './routes/categoryRoutes.js'; // Import the category routes
+import tagRoutes from './routes/tagRoutes.js'
+import blogRoutes from './routes/blogRoutes.js'
+import cors from "cors";
+
+
+dotenv.config();
+connectDB();
+
+
+const app = express();
+
+const allowedOrigins = new Set([
+  "http://localhost:3000",
+  "http://localhost:5173",
+
+]);
+
+// CORS middleware setup
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // If origin is undefined (like Postman or curl), allow it
+      
+      if (!origin || allowedOrigins.has(origin)) {
+        callback(null, true);
+        // console.log("Origin:", origin);
+      } else {
+        console.warn("Blocked CORS request from:", origin);
+        callback(new Error("CORS not allowed for this origin"));
+      }
+    },
+    credentials: true, // Allows cookies and session headers
+  })
+);
+
+
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/catagory", categoryRoutes);
+app.use("/api/tag", tagRoutes);
+app.use("/api/blog", blogRoutes);
+
+
+
+
+
+const PORT = process.env.PORT || 5006;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
